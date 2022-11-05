82a936135667d45b915464894a13b631b0b9adbb
Mdhossain60 <110592017+Mdhossain60@users.noreply.github.com>
2022-11-05 22:50:00 +0600

    Add files via upload

---
+++ js/app.js
@@ -0,0 +1,21 @@
+   let eyeIcon = document.querySelector(".eye");
+      let input = document.querySelector(".inputTag");
+   eyeIcon.addEventListener('click',showPassword);
+
+ function showPassword() {
+   
+   if(  input .type == 'password'){
+    input .type = 'text';
+   } else{
+    input .type = 'password';
+   }
+
+
+
+ }
+
+
+
+
+
+
